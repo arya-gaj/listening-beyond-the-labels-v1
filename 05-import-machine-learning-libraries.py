5736d3aee91cb98adab1d93e75301b2218380321
Aryaman Gajrani <ugs22091_it.aryaman@cbit.org.in>
2025-06-23 15:47:17 +0530

    Create 05-import-machine-learning-libraries.py

---
+++ 05-import-machine-learning-libraries.py
@@ -0,0 +1,4 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
+from sklearn.model_selection import train_test_split
