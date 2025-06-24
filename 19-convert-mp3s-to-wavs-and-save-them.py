35862b3d829cc630c8d920ffceb772661340312a
Aryaman Gajrani <ugs22091_it.aryaman@cbit.org.in>
2025-06-25 00:57:05 +0530

    Create 19-convert-mp3s-to-wavs-and-save-them.py

---
+++ 19-convert-mp3s-to-wavs-and-save-them.py
@@ -0,0 +1,3 @@
+convert_all_mp3s(dementia_src, "/content/processed-wav/dementia", max_threads=16)
+convert_all_mp3s(control_src, "/content/processed-wav/control", max_threads=16)
+convert_all_mp3s(unlabeled_src, "/content/processed-wav/unlabeled", max_threads=16)
