623c5cc5d4c2ecde956386e9ad8cd13f295bebe6
Aryaman Gajrani <ugs22091_it.aryaman@cbit.org.in>
2025-06-25 00:56:22 +0530

    Create 18-set-source-paths-for-original-mp3-files.py

---
+++ 18-set-source-paths-for-original-mp3-files.py
@@ -0,0 +1,3 @@
+dementia_src = "/content/data/labeled-dementia/Labeled (Dementia)"
+control_src = "/content/data/labeled-control/Labeled (Control)"
+unlabeled_src = "/content/data/unlabeled/Unlabeled"
