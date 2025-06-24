dfb06a546fb6bfcfcf0b5b9a0320e26845e674c1
Aryaman Gajrani <ugs22091_it.aryaman@cbit.org.in>
2025-06-24 18:40:22 +0530

    Create 12-set-dataset-folder-paths.py

---
+++ 12-set-dataset-folder-paths.py
@@ -0,0 +1,3 @@
+control_path = "/content/data/labeled-control/Labeled (Control)"
+dementia_path = "/content/data/labeled-dementia/Labeled (Dementia)"
+unlabeled_path = "/content/data/unlabeled/Unlabeled"
