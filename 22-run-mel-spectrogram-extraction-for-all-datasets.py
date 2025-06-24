360cf1af76459ddce93ca054fb2114ff75726d2b
Aryaman Gajrani <ugs22091_it.aryaman@cbit.org.in>
2025-06-25 01:14:17 +0530

    Create 22-run-mel-spectrogram-extraction-for-all-datasets.py

---
+++ 22-run-mel-spectrogram-extraction-for-all-datasets.py
@@ -0,0 +1,3 @@
+extract_all_mels("/content/processed-wav/dementia", "/content/processed-specs/dementia")
+extract_all_mels("/content/processed-wav/control", "/content/processed-specs/control")
+extract_all_mels("/content/processed-wav/unlabeled", "/content/processed-specs/unlabeled")
