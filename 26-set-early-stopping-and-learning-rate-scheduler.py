537d45910e15f2aa604104c6975f7115dadd693f
Aryaman Gajrani <ugs22091_it.aryaman@cbit.org.in>
2025-07-02 00:51:33 +0530

    Update 26-set-early-stopping-and-learning-rate-scheduler.py

--- 26-set-early-stopping-and-learning-rate-scheduler.py
+++ 26-set-early-stopping-and-learning-rate-scheduler.py
@@ -1,2 +1,23 @@
+"""
+Listening Beyond the Labels – A scalable and non-invasive speech-based machine learning model for early Alzheimer's detection using mel-spectrograms and lightweight semi-supervised CNN with no transcription or neuroimaging needed.
+
+Copyright (C) 2025 Aryaman Gajrani
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+Contact: arya-gaj@proton.me
+"""
+
 early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
 lr_scheduler = ReduceLROnPlateau(monitor='val_loss', patience=2, factor=0.5, min_lr=0.00001)
