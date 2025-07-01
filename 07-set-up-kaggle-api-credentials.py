fb456e4f37bf88dbd000245eb8760ff8d6159f97
Aryaman Gajrani <ugs22091_it.aryaman@cbit.org.in>
2025-07-02 00:00:46 +0530

    Update 07-set-up-kaggle-api-credentials.py

--- 07-set-up-kaggle-api-credentials.py
+++ 07-set-up-kaggle-api-credentials.py
@@ -1,7 +1,3 @@
-os.makedirs('/root/.kaggle', exist_ok=True)
-shutil.move('kaggle.json', '/root/.kaggle/kaggle.json')
-os.chmod('/root/.kaggle/kaggle.json', 600)
-
 """
 Listening Beyond the Labels – A scalable and non-invasive speech-based machine learning model for early Alzheimer's detection using mel-spectrograms and lightweight semi-supervised CNN with no transcription or neuroimaging needed.
 
@@ -22,3 +18,7 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 Contact: arya-gaj@proton.me
 """
+
+os.makedirs('/root/.kaggle', exist_ok=True)
+shutil.move('kaggle.json', '/root/.kaggle/kaggle.json')
+os.chmod('/root/.kaggle/kaggle.json', 600)
