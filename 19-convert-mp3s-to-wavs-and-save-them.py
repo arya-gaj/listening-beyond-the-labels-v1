83e75d22759a729d93721e46f39d990f5dc0b124
Aryaman Gajrani <ugs22091_it.aryaman@cbit.org.in>
2025-07-02 00:29:00 +0530

    Update 19-convert-mp3s-to-wavs-and-save-them.py

--- 19-convert-mp3s-to-wavs-and-save-them.py
+++ 19-convert-mp3s-to-wavs-and-save-them.py
@@ -1,3 +1,24 @@
+"""
+Listening Beyond the Labels – A scalable and non-invasive speech-based machine learning model for early Alzheimer's detection using mel-spectrograms and lightweight semi-supervised CNN with no transcription or neuroimaging needed.
+
+Copyright (C) 2025 Aryaman Gajrani
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+Contact: arya-gaj@proton.me
+"""
+
 convert_all_mp3s(dementia_src, "/content/processed-wav/dementia", max_threads=16)
 convert_all_mp3s(control_src, "/content/processed-wav/control", max_threads=16)
 convert_all_mp3s(unlabeled_src, "/content/processed-wav/unlabeled", max_threads=16)
