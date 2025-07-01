e7c18052011bc715f399ae7ac3130f8d782e9920
Aryaman Gajrani <ugs22091_it.aryaman@cbit.org.in>
2025-07-02 00:47:52 +0530

    Update 20-define-function-to-extract-mel-spectrogram-from-audio.py

--- 20-define-function-to-extract-mel-spectrogram-from-audio.py
+++ 20-define-function-to-extract-mel-spectrogram-from-audio.py
@@ -1,3 +1,24 @@
+"""
+Listening Beyond the Labels – A scalable and non-invasive speech-based machine learning model for early Alzheimer's detection using mel-spectrograms and lightweight semi-supervised CNN with no transcription or neuroimaging needed.
+
+Copyright (C) 2025 Aryaman Gajrani
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+Contact: arya-gaj@proton.me
+"""
+
 def extract_mel_spec(file_path, sr=16000, n_mels=128, duration=5):
     y, _ = librosa.load(file_path, sr=sr, duration=duration)
     mel_spec = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=n_mels)
