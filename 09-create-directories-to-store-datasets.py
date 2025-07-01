0366c8e7433d4f5a90acd625a008f4b42da21cf7
Aryaman Gajrani <ugs22091_it.aryaman@cbit.org.in>
2025-07-02 00:11:10 +0530

    Update 09-create-directories-to-store-datasets.py

--- 09-create-directories-to-store-datasets.py
+++ 09-create-directories-to-store-datasets.py
@@ -1,7 +1,3 @@
-!mkdir -p data/labeled-dementia
-!mkdir -p data/labeled-control
-!mkdir -p data/unlabeled
-
 """
 Listening Beyond the Labels – A scalable and non-invasive speech-based machine learning model for early Alzheimer's detection using mel-spectrograms and lightweight semi-supervised CNN with no transcription or neuroimaging needed.
 
@@ -22,3 +18,7 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 Contact: arya-gaj@proton.me
 """
+
+!mkdir -p data/labeled-dementia
+!mkdir -p data/labeled-control
+!mkdir -p data/unlabeled
