2dfe2559b1537eb2d87477f52796a682b27a9564
Aryaman Gajrani <ugs22091_it.aryaman@cbit.org.in>
2025-07-02 01:04:08 +0530

    Update 43-print-pseudo-accuracy-percentage.py

--- 43-print-pseudo-accuracy-percentage.py
+++ 43-print-pseudo-accuracy-percentage.py
@@ -1,1 +1,22 @@
+"""
+Listening Beyond the Labels – A scalable and non-invasive speech-based machine learning model for early Alzheimer's detection using mel-spectrograms and lightweight semi-supervised CNN with no transcription or neuroimaging needed.
+
+Copyright (C) 2025 Aryaman Gajrani
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+Contact: arya-gaj@proton.me
+"""
+
 print(f"Pseudo Accuracy (confident predictions): {pseudo_accuracy * 100:.2f}%")
