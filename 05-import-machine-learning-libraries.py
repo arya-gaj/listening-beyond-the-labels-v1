bbd7487c763bed2d103dd0ab269a0da6b519530a
Aryaman Gajrani <ugs22091_it.aryaman@cbit.org.in>
2025-07-01 23:11:44 +0530

    Update 05-import-machine-learning-libraries.py

--- 05-import-machine-learning-libraries.py
+++ 05-import-machine-learning-libraries.py
@@ -2,3 +2,24 @@ import tensorflow as tf
 from tensorflow.keras import layers, models
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
 from sklearn.model_selection import train_test_split
+
+"""
+Listening Beyond the Labels – A scalable and non-invasive speech-based machine learning model for early Alzheimer's detection using mel-spectrograms and lightweight semi-supervised CNN with no transcription or neuroimaging needed.
+
+Copyright (C) 2025 Aryaman Gajrani
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+Contact: arya-gaj@proton.me
+"""
